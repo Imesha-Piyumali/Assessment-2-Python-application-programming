68ccde1fa5775d4b7d91309939f789c17729a3c6
Imesha-Piyumali <u3305049@uni.canberra.edu.au>
2025-09-17 22:20:30 +1000

    answers
    answer

---
+++ answer.py
@@ -0,0 +1,893 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+==================== RESTART: D:/UC/IIT/Assignments/cafe.py ====================
+
+--- Campus Café POS ---
+1. Show Menu
+2. Add Item
+3. View Cart
+4. Checkout
+5. Exit
+Choice: Coffee
+Invalid choice.
+
+--- Campus Café POS ---
+1. Show Menu
+2. Add Item
+3. View Cart
+4. Checkout
+5. Exit
+Choice: 1
+
+=== Café Menu ===
+Coffee - $3.50 (Drink)
+Tea - $2.50 (Drink)
+Muffin - $2.00 (Food)
+Sandwich - $5.00 (Food)
+Juice - $4.00 (Drink)
+Salad - $6.50 (Food)
+
+--- Campus Café POS ---
+1. Show Menu
+2. Add Item
+3. View Cart
+4. Checkout
+5. Exit
+Choice: 2
+Enter item name: Coffee
+Added Coffee to cart.
+
+--- Campus Café POS ---
+1. Show Menu
+2. Add Item
+3. View Cart
+4. Checkout
+5. Exit
+Choice: 4
+Apply student discount (y/n)? y
+
+=== Receipt ===
+Coffee     $3.50
+
+Subtotal: $3.50
+Tax (10%): $0.35
+Student Discount: -$0.19
+Total: $3.66
+
+--- Campus Café POS ---
+1. Show Menu
+2. Add Item
+3. View Cart
+4. Checkout
+5. Exit
+Choice: 3
+Cart is empty.
+
+--- Campus Café POS ---
+1. Show Menu
+2. Add Item
+3. View Cart
+4. Checkout
+5. Exit
+Choice: 1
+
+=== Café Menu ===
+Coffee - $3.50 (Drink)
+Tea - $2.50 (Drink)
+Muffin - $2.00 (Food)
+Sandwich - $5.00 (Food)
+Juice - $4.00 (Drink)
+Salad - $6.50 (Food)
+
+--- Campus Café POS ---
+1. Show Menu
+2. Add Item
+3. View Cart
+4. Checkout
+5. Exit
+Choice: 3
+Cart is empty.
+
+--- Campus Café POS ---
+1. Show Menu
+2. Add Item
+3. View Cart
+4. Checkout
+5. Exit
+Choice: 2
+Enter item name: Tea
+Added Tea to cart.
+
+--- Campus Café POS ---
+1. Show Menu
+2. Add Item
+3. View Cart
+4. Checkout
+5. Exit
+Choice: 3
+
+=== Your Cart ===
+1. Tea - $2.50
+Total items: 1
+
+--- Campus Café POS ---
+1. Show Menu
+2. Add Item
+3. View Cart
+4. Checkout
+5. Exit
+Choice: 
+============ RESTART: D:/UC/IIT/Assignments/Campus Café Checkout.py ============
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 1
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: Coffee
+Invalid option.
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 1
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 2
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+Enter item name (or 'back'): Coffee
+Enter quantity: 2
+Added 2x coffee
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 2
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+Enter item name (or 'back'): Muffin
+Enter quantity: 1
+Added 1x muffin
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 3
+
+--- Cart ---
+2x Coffee = $7.00
+1x Muffin = $4.00
+Categories in cart: {'food', 'drink'}
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 4
+
+--- Receipt ---
+2x Coffee     = $7.00
+1x Muffin     = $4.00
+Meal deal: -$1.00
+
+Subtotal: $10.00
+Tax (10%): $1.00
+Are you a student (y/n)? y
+Student discount: -$0.55
+Total: $10.45
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 4
+Cart is empty.
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 5
+Goodbye!
+
+= RESTART: D:/UC/IIT/Assignments/Case Study 3_A Boolean Circuit Equivalence.py =
+Enter values for A, B, C (0 or 1). Type 'q' to quit.
+A B C:  0 0 0
+Inputs: A=0, B=0, C=0 => X=0, Y=0
+A B C:  0 0 1
+Inputs: A=0, B=0, C=1 => X=0, Y=0
+A B C:  1 1 1
+Inputs: A=1, B=1, C=1 => X=1, Y=1
+A B C: 
+= RESTART: D:/UC/IIT/Assignments/Smart Classroom Monitor_Python Implementation.py
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 1
+Projector is now ON
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 2
+Enter topic: 3
+Topic set to: 3
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 4
+Enter student name to remove: Imesha
+Student not found.
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 3
+Enter student name: Imesha
+Imesha checked in.
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 5
+Enter temperature (°C): 15
+⚠️ ALERT: Temperature out of range!
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 6
+
+--- Classroom Report ---
+Projector: ON
+Capacity: 1/3
+Topic: 3
+Students: {'Imesha'}
+Temperatures → Min: 15.0, Max: 15.0, Avg: 15.0
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 7
+Exiting...
+
+= RESTART: D:/UC/IIT/Assignments/Smart Classroom Monitor_Python Implementation.py
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 
+= RESTART: D:/UC/IIT/Assignments/Smart Classroom Monitor_Python Implementation.py
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 
+======================================================================================== RESTART: D:/UC/IIT/Assignments/Campus Café Checkout.py ========================================================================================
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 1
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: Coffee
+Invalid option.
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 5
+Goodbye!
+
+======================================================================================== RESTART: D:/UC/IIT/Assignments/Campus Café Checkout.py ========================================================================================
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 1
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 2
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+Enter item name (or 'back'): Coffee
+Enter quantity: 2
+Added 2x coffee
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 2
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+Enter item name (or 'back'): Muffin
+Enter quantity: 2
+Added 2x muffin
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 3
+
+--- Cart ---
+2x Coffee = $7.00
+2x Muffin = $8.00
+Categories in cart: {'drink', 'food'}
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 4
+
+--- Receipt ---
+2x Coffee     = $7.00
+2x Muffin     = $8.00
+Meal deal: -$1.00
+
+Subtotal: $14.00
+Tax (10%): $1.40
+Are you a student (y/n)? y
+Student discount: -$0.77
+Total: $14.63
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 5
+Goodbye!
+
+Warning (from warnings module):
+  File "D:/UC/IIT/Assignments/Campus Café Checkout.py", line 40
+    print("\nCart is e\Empty!")
+SyntaxWarning: invalid escape sequence '\E'
+>>> 
+======================================================================================== RESTART: D:/UC/IIT/Assignments/Campus Café Checkout.py ========================================================================================
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 
+======================================================================================== RESTART: D:/UC/IIT/Assignments/Campus Café Checkout.py ========================================================================================
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 
+============================================================================= RESTART: D:/UC/IIT/Assignments/Case Study 3_A Boolean Circuit Equivalence.py =============================================================================
+Enter values for A, B, C (0 or 1). Type 'q' to quit.
+A B C:  0 0 0
+Inputs: A=0, B=0, C=0 => X=0, Y=0
+A B C: 
+= RESTART: D:/UC/IIT/Assignments/Case Study 3_A Boolean Circuit Equivalence.py =
+Enter values for A, B, C (0 or 1). Type 'q' to quit.
+A B C:  1 1 1
+Traceback (most recent call last):
+  File "D:/UC/IIT/Assignments/Case Study 3_A Boolean Circuit Equivalence.py", line 35, in <module>
+    a, b = circuit_outputs(A, B, C)
+  File "D:/UC/IIT/Assignments/Case Study 3_A Boolean Circuit Equivalence.py", line 20, in circuit_outputs
+    return a, b
+NameError: name 'a' is not defined. Did you mean: 'A'?
+
+=============== RESTART: D:/UC/IIT/Assignments/Case Study 3_A Boolean Circuit Equivalence.py ===============
+Enter values for A, B, C (0 or 1). Type 'q' to quit.
+A B C:  1 1 1
+Traceback (most recent call last):
+  File "D:/UC/IIT/Assignments/Case Study 3_A Boolean Circuit Equivalence.py", line 35, in <module>
+    a, b = circuit_outputs(A, B, C)
+  File "D:/UC/IIT/Assignments/Case Study 3_A Boolean Circuit Equivalence.py", line 20, in circuit_outputs
+    return a, b
+NameError: name 'b' is not defined. Did you mean: 'B'?
+
+=============== RESTART: D:/UC/IIT/Assignments/Case Study 3_A Boolean Circuit Equivalence.py ===============
+Enter values for A, B, C (0 or 1). Type 'q' to quit.
+A B C:  0 0 0
+Inputs: A=0, B=0, C=0 => X=0, Y=0
+A B C:  0 0 1
+Inputs: A=0, B=0, C=1 => X=0, Y=0
+A B C:  0 1 0
+Inputs: A=0, B=1, C=0 => X=0, Y=0
+A B C:  0 1 1
+Inputs: A=0, B=1, C=1 => X=0, Y=0
+A B C:  1 0 0
+Inputs: A=1, B=0, C=0 => X=1, Y=1
+A B C:  1 0 1
+Inputs: A=1, B=0, C=1 => X=1, Y=1
+A B C:  1 1 0
+Inputs: A=1, B=1, C=0 => X=0, Y=0
+A B C:  1 1 1
+Inputs: A=1, B=1, C=1 => X=1, Y=1
+A B C:  0 0 0
+Inputs: A=0, B=0, C=0 => X=0, Y=0
+A B C: 
+============================================================================= RESTART: D:/UC/IIT/Assignments/Case Study 3_A Boolean Circuit Equivalence.py =============================================================================
+Enter values for A, B, C (0 or 1). Type 'q' to quit.
+A B C: 
+================================================================================================ RESTART: D:/UC/IIT/Assignments/cafe.py ================================================================================================
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 1
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: Coffee
+Invalid option.
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 1
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 2
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+Enter item name (or 'back'): Coffee
+Enter quantity: 2
+Added 2x coffee
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 1
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: Sandwich
+Invalid option.
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 2
+
+--- Menu ---
+Coffee     $3.50 (drink)
+Tea        $2.50 (drink)
+Muffin     $4.00 (food)
+Sandwich   $6.50 (food)
+Juice      $3.00 (drink)
+Cookie     $2.00 (food)
+Enter item name (or 'back'): Sandwich
+Enter quantity: 2
+Added 2x sandwich
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 3
+
+--- Cart ---
+2x Coffee = $7.00
+2x Sandwich = $13.00
+Categories in cart: {'food', 'drink'}
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 4
+
+--- Receipt ---
+2x Coffee     = $7.00
+2x Sandwich   = $13.00
+Meal deal: -$1.00
+
+Subtotal: $19.00
+Tax (10%): $1.90
+Are you a student (y/n)? y
+Student discount: -$1.04
+Total: $19.85
+
+--- Main Menu ---
+1. Show menu
+2. Add item
+3. View cart
+4. Checkout
+5. Exit
+Choice: 5
+Goodbye!
+
+====== RESTART: D:/UC/IIT/Assignments/Smart Classroom Monitor_Python Implementation.py ======
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 1
+Projector is now ON
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 2
+Enter topic: Math
+Topic set to: Math
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 3
+Enter student's name: Ashan
+Ashan checked in.
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 3
+Enter student's name: Malsha
+Malsha checked in.
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 3
+Enter student's name: John
+John checked in.
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 3
+Enter student's name: Ann
+Ann checked in.
+⚠️ ALERT: ROOM FULL!
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 5
+Enter Temperature (°C): 25,30,12
+Invalid Temperature!
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 20,15,30
+Invalid option!
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 5
+Enter Temperature (°C): 25
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 5
+Enter Temperature (°C): 15
+⚠️ ALERT: Temperature is out of range!
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 5
+Enter Temperature (°C): 12
+⚠️ ALERT: Temperature is out of range!
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 5
+Enter Temperature (°C): 27
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 6
+
+--- Classroom Report ---
+Projector: ON
+Capacity: 4/3
+Topic: Math
+Students: {'John', 'Malsha', 'Ann', 'Ashan'}
+Temperatures → Min: 12.0, Max: 27.0, Avg: 19.8
+
+--- Main Menu ---
+1. Toggle projector
+2. Set topic
+3. Add student
+4. Remove student
+5. Add temperature reading
+6. Show report
+7. Exit
+Choice: 7
+Exiting...
